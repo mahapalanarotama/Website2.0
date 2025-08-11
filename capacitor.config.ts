4f64b33af0c8dfa37708e1e089456b7ec71fa071
Muhammad Fairus Fawas Afanza <mufaza2408@gmail.com>
2025-08-11 20:29:18 +0700

    #

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'id.mahapalanarotama.app',
+  appName: 'Mahapala Narotama App',
+  webDir: 'client/dist'
+};
+
+export default config;
