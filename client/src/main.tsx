4845d6b53d566f152cb6285fda8630f8029e1b8e
Muhammad Fairus Fawas Afanza <mufaza2408@gmail.com>
2025-08-27 15:11:42 +0700

    feat: wrap App component with BrowserRouter for routing support

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,13 @@ import App from "./App";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
